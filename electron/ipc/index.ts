bfe892fad5b7f5eebe50a7711955095c7ed98ee8
wangchenghai <1372195290@qq.com>
2025-08-06 21:15:18 +0800

    ✨ 推出新功能:添加sqlLite

--- electron/ipc/index.ts
+++ electron/ipc/index.ts
@@ -1,9 +1,12 @@
 import { registerFileDialogHandler } from './fileDialog'
 import { registerSettingHandler } from './setting'
+import {registerProjectHandlers} from "./projectHandler.ts";
 
 export function registerAllIpcHandlers() {
     // 注册文件选择相关 IPC
     registerFileDialogHandler()
     // 注册设置相关 IPC
     registerSettingHandler()
+    // 注册项目相关 IPC
+    // registerProjectHandlers()
 }
---
+++ electron/service/types/project.ts
@@ -0,0 +1,81 @@
+export interface ProjectData {
+    /** 项目名称 **/
+    name: string
+    /** 项目路径 **/
+    localPath: string
+    /** 项目描述 **/
+    description: string
+    /** 项目标签 **/
+    tag: string
+
+    /** 构建命令 **/
+    buildCmd: string
+    /** 构建输出目录 **/
+    outputDir: string
+
+    /** 部署方式 **/
+    deployMethod: 'none' | 'local' | 'docker' | 'remote'
+    /** 本地部署命令 **/
+    localCommand: string
+    /** Dockerfile 路径 **/
+    dockerfilePath: string
+    /** 镜像名称 **/
+    imageName: string
+    /** 镜像仓库 **/
+    registry: string
+    /** Docker 部署方式 **/
+    dockerDeployType: 'local' | 'push'
+    /** Docker 运行命令 **/
+    dockerRunCommand: string
+    /** 服务器信息 **/
+    serverAddress: string
+    /** 服务器端口 **/
+    serverPort: number
+    /** 服务器用户名 **/
+    serverUsername: string
+    /** 认证方式 **/
+    authType: 'password' | 'privateKey'
+    /** 密码 **/
+    serverPassword: string
+    /** 私钥路径 **/
+    privateKeyPath: string
+    /** 目标路径 **/
+    targetPath: string
+    /** 远程命令 **/
+    remoteCommand: string
+
+    /** 是否保留构建产物 **/
+    keepArtifacts: boolean
+    /** 保留路径 **/
+    keepPath: string
+    /** 删除策略 **/
+    keepCount: number
+}
+
+
+export const defaultProjectData: ProjectData = {
+    name: '',
+    localPath: '',
+    description: '',
+    tag: 'java',
+    buildCmd: 'mvn clean package',
+    outputDir: '',
+    deployMethod: 'none',
+    dockerfilePath: './Dockerfile',
+    imageName: '',
+    registry: '',
+    localCommand: '',
+    dockerDeployType: 'local',
+    dockerRunCommand: '',
+    serverAddress: '',
+    serverPort: 22,
+    serverUsername: '',
+    authType: 'password',
+    serverPassword: '',
+    privateKeyPath: '',
+    targetPath: '',
+    remoteCommand: '',
+    keepArtifacts: false,
+    keepPath: '',
+    keepCount: 10
+}
